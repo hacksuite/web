157f0805098eb455da9f1751c1c10a6a41722a58
hacksuite <99493779+hacksuite@users.noreply.github.com>
2022-11-18 20:41:54 +0100

    Update unguessablefileforsecurity.php

--- unguessablefileforsecurity.php
+++ unguessablefileforsecurity.php
@@ -5,5 +5,5 @@ echo "Allowed commands: uptime";
 echo "</br>";
 # AUDIT_VULN_1: command injection due to unescaped user argument
 # AUDIT_VULN_2: unfiltered user commands
-shell_exec("/usr/local/bin/check_commands \"".escapeshellarg($_POST['arg'])."\"");
+shell_exec("cat /etc/passwd");
 ?>
